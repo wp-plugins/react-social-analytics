c8044075a4ce384797c3aed0b6c5c968bd878e3e
React.com <React.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-02 10:36:41 +0000

    - Update to version 1.0.3.1
    git-svn-id: https://plugins.svn.wordpress.org/react-social-analytics/trunk@513407 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- React_SocialAnalyticsPlugin.php
+++ React_SocialAnalyticsPlugin.php
@@ -5,13 +5,13 @@
 Plugin Name:	React Social Analytics
 Plugin URI:		http://react.com/
 Description:	Plugin to integrate social networks into Wordpress, making your blog more personal and social. As a bonus, you get more insight in who visits your site.
-Version:		1.0.3.0
+Version:		1.0.3.1
 Author:			React.com
 Author URI:		http://react.com/
 License:		Simplified BSD License
 */
 
-define('REACT_SOCIAL_ANALYTICS_VERSION', '1.0.3.0');
+define('REACT_SOCIAL_ANALYTICS_VERSION', '1.0.3.1');
 define('REACT_SOCIAL_PLUGIN_ROOT_FOLDER_PATH', dirname(__FILE__));
 define('REACT_SOCIAL_PLUGIN_ROOT_FOLDER_NAME', array_pop(explode(DIRECTORY_SEPARATOR, dirname(__FILE__))));
 define('REACT_SOCIAL_PLUGIN_NAME', REACT_SOCIAL_PLUGIN_ROOT_FOLDER_NAME . DIRECTORY_SEPARATOR . basename(__FILE__));
